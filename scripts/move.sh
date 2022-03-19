60b2407b048819b74a153d4e4c772c288b93ba26
tejaswini kakade <92703176+tejaswinikakade@users.noreply.github.com>
2022-03-19 21:01:13 +0530

    Create move.sh

---
+++ scripts/move.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+   sudo mv /home/ubuntu/chatapp/.idea /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/chatapp/.github /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/chatapp/appspec.yml /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/chatapp/script /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/chatapp/fundoo /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/chatapp/requirements.txt /home/ubuntu/new_chatapp
